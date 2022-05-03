8ec4d4aa1f78ad597b577e50363aa3ce69b5e5c8
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-03 15:08:27 +0530

    Add gesture to each card and Navigate to RecipeDetail screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -45,7 +46,19 @@ class _MyHomePageState extends State<MyHomePage> {
           child: ListView.builder(
               itemCount: Recipe.samples.length,
               itemBuilder: (BuildContext context, int index) {
-                return buildRecipeCard(Recipe.samples[index]);
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return RecipeDetail(recipe: Recipe.samples[index]);
+                        },
+                      ),
+                    );
+                  },
+                  child: buildRecipeCard(Recipe.samples[index]),
+                );
               }),
         ));
   }
